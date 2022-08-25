d2c8c5c27423d54d2fc5ef33b2e6a7eab005aed6
WardVanSteenwinkel <r0856666@student.thomasmore.be>
2022-08-25 09:35:51 +0200

    queu problem works

--- .idea/js/queu.js
+++ .idea/js/queu.js
@@ -1,22 +1,31 @@
 function queueTime(customers, n) {
-    //array voor bijhouden kassa'a
-    let arr = [];
-    //aanmaken initiele kassa's
-    for (let i = 0; i < n; i++) {
-        let kassa = 0;
-        kassa += customers[i];
-        arr.push(kassa);
-    }
-    //neem kassa dat eerste klaar is en geef deze de volgende klant
-    for (let i = n; i < customers.length; i++) {
+    //kassa's > klanten
+    if(n>customers.length && customers.length!==0){
+        customers.sort(function(a, b){return a - b});
+        return customers[customers.length-1];
+    }else if(customers.length===0){
+        return 0;
+    }else{
+        //array voor bijhouden kassa'a
+        let arr = [];
+        //aanmaken initiele kassa's
+        for (let i = 0; i < n; i++) {
+            let kassa = 0;
+            kassa += customers[i];
+            arr.push(kassa);
+        }
+        //neem kassa dat eerste klaar is en geef deze de volgende klant
+        for (let i = n; i < customers.length; i++) {
+            arr.sort(function(a, b){return a - b});
+            arr[0] += customers[i];
+        }
         arr.sort(function(a, b){return a - b});
-        arr[0] += customers[i];
+        return arr[n-1];
     }
-    arr.sort(function(a, b){return a - b});
-    return arr[0];
 }
 
 
 
 
-console.log(queueTime([10,2,3,3]),2);
+
+console.log(queueTime([],3));
